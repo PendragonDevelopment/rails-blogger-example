aaba9b8a1c12a63e3e2742c3d4b39f3ac326bf32
dmcbrayer <dmcbrayer@gmail.com>
2016-02-04 19:55:02 -0500

    Adds conditionals to the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -14,7 +14,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.create(article_params)
-    redirect_to @article
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,18 +27,29 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update_attributes(article_params)
-    redirect_to @article
+    
+
+    if @article.update_attributes(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to articles_path
+
+    if @article.destroy
+      redirect_to articles_path
+    else
+      redirect_to @article
+    end
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
