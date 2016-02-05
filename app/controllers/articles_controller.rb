cddd023ef55b51829c753e7fc5063d077f085ce1
dmcbrayer <dmcbrayer@gmail.com>
2016-02-04 19:25:57 -0500

    Added an edit button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
+  
   def new
     @article = Article.new
   end
@@ -27,9 +27,15 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
 end
